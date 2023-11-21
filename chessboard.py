8121a56587bfc1b9f09c7b41d632e37045e208d5
Parker Clark <69479105+tundrv0l@users.noreply.github.com>
2023-11-21 15:02:08 -0600

    init files, drivers

---
+++ chessboard.py
@@ -0,0 +1,10 @@
+'''----------------- 
+# Author: Parker Clark
+# Date: 11/21/2023
+# Description: A driver for chess board.  
+-----------------'''
+
+import chess
+
+
+board = chess.Board()
---
+++ main.py
@@ -0,0 +1,14 @@
+'''----------------- 
+# Author: Parker Clark
+# Date: 11/21/2023
+# Description: A driver for the N of Queens Problem.  
+-----------------'''
+
+def main():
+    pass
+
+
+if __name__=='__main__':
+    main()
+
+
