5563c25fad24db39863a787111db2eb21bcd6184
tundrv0l <tundravol2@gmail.com>
2023-11-22 11:50:02 -0600

    Fixes

--- chessboard.py
+++ chessboard.py
@@ -5,6 +5,29 @@
 -----------------'''
 
 import chess
+import pygame
 
 
-board = chess.Board()
+WHITE = (251, 235, 193)
+BLACK = (157, 108, 62)
+
+pieces = {}
+pieces["Q"] = pygame.image.load("images/bQ.png")
+
+class ChessBoard:
+    def __init__(self): # Class constructor
+        self.board = chess.Board()
+
+    def draw(self, screen, game_state):
+
+        self.board.clear()
+
+        # Draw the board
+        for row in range(8):
+            for col in range(8):
+                pygame.draw.rect(screen, WHITE if (row + col) % 2 == 0 else BLACK, pygame.Rect(col*100, row*100, 100, 100))
+                if game_state[row][col] == 'Q':
+                    screen.blit(pieces['Q'], (col * 100, row * 100))
+
+"""         # Draw the queen piece
+        screen.blit(pieces["Q"], (3 * 100, 3 * 100)) """
--- main.py
+++ main.py
@@ -4,11 +4,72 @@
 # Description: A driver for the N of Queens Problem.  
 -----------------'''
 
-def main():
-    pass
+import pygame
+from chessboard import ChessBoard
+import random
 
+""" def is_safe(board: ChessBoard , row: int, col: int)->bool:
+    # Check row on left side
+    for i in range(col):
+        if board[row][i] == 'Q':
+            return False
+
+    # Check upper diagonal on left side
+    for i, j in zip(range(row, -1, -1), range(col, -1, -1)):
+        if board[i][j] == 'Q':
+            return False
+
+    # Check lower diagonal on left side
+    for i, j in zip(range(row, len(board)), range(col, -1, -1)):
+        if board[i][j] == 'Q':
+            return False
+
+    return True """
+
+
+def n_queens_solution(n, board=[], row=0):
+    if row == n:
+        return [board]
+
+
+    solutions = []
+    for col in range(n):
+        if all(col != c and row-r != abs(col-c) for r, c in enumerate(board)):
+            for result in n_queens_solution(n, board + [col], row + 1):
+                solutions.append(result)
+
+    return solutions
+
+
+def main():
+    pygame.init()
+    screen = pygame.display.set_mode((800, 800))
+
+    clock = pygame.time.Clock()
+    pygame.display.set_caption("N of Queens")
+
+    chessboard = ChessBoard()  # Create an instance of the ChessBoard class
+
+    done = False
+
+    game_state = [['' for _ in range(8)] for _ in range(8)]
+
+    # update the board state with n of queens
+    solutions = n_queens_solution(8)
+    if solutions:
+        solution = random.choice(solutions)
+        for row, col in enumerate(solution):
+            game_state[row][col] = 'Q'
+
+    while not done:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                done = True
+            
+        chessboard.draw(screen, game_state)  # Draw the chessboard with updated game state
+        pygame.display.flip()  # Update the display
+        
+    pygame.quit()
 
 if __name__=='__main__':
     main()
-
-
